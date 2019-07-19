0a29c568727b73bef47f2c43a094111778e700f3
Tia Rose <shortyshort789@aol.com>
2019-07-19 15:47:38 -0700

    added hooks to json header and response

--- src/components/jsonview/header.js
+++ src/components/jsonview/header.js
@@ -1,36 +1,36 @@
-import React from 'react';
-// import React, { useContext } from "react";
+// import React from 'react';
+import React, { useContext } from "react";
 import ReactJson from "react-json-view";
-// import { RestyContext } from "../../context/restyContext";
+import { RestyContext } from "../../context/restyContext";
 
 /**
  * JsonHeader class/component
  * @description uses the react-json-view component to render JSON headers
  */
 
-class JsonHeader extends React.Component {
-  render() {
-    return (
-      <ReactJson
-        name="Headers"
-        enableClipboard={false}
-        collapsed={true}
-        src={this.props.src}
-      />
-    );
-  }
-}
+// class JsonHeader extends React.Component {
+//   render() {
+//     return (
+//       <ReactJson
+//         name="Headers"
+//         enableClipboard={false}
+//         collapsed={true}
+//         src={this.props.src}
+//       />
+//     );
+//   }
+// }
 
-// const JsonHeader = () => {
-//   const state = useContext(RestyContext);
-//   return (
-//     <ReactJson
-//       name="Headers"
-//       enableClipboard={false}
-//       collapsed={true}
-//       src={state.header}
-//     />
-//   );
-// };
+const JsonHeader = () => {
+  const state = useContext(RestyContext);
+  return (
+    <ReactJson
+      name="Headers"
+      enableClipboard={false}
+      collapsed={true}
+      src={state.header}
+    />
+  );
+};
 
 export default JsonHeader;
